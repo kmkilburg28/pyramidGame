7e50eb38341f12186622796a20ffbefbc5191cd9
zahydeem <44513031+zahydeem@users.noreply.github.com>
2018-10-27 12:39:02 -0400

    enemydestructibles

---
+++ pyramidGame/Assets/Scripts/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+    public GameObject destroyedVersion;
+
+     void OnMouseDown()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(transform.parent.gameObject);
+    }
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
