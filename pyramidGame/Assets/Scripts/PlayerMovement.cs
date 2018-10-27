196bfa6ef4111955136636ea2ad1e0eb28da4f7a
Kaden Kilburg <kmkilburg28@gmail.com>
2018-10-26 20:47:31 -0500

    Player worksish

--- pyramidGame/Assets/Scripts/PlayerMovement.cs
+++ pyramidGame/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public float moveVelocity;
+
+    Rigidbody thisRB;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        thisRB = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        thisRB.AddForce(
+            horizontalInput * moveVelocity, 
+            0f,
+            verticalInput * moveVelocity,
+            ForceMode.Force
+        );
+    }
 }
