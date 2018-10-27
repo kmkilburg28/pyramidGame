f681813a8fec9aba69b57d5d8e1ac5d0d41901ed
Kaden Kilburg <kmkilburg28@gmail.com>
2018-10-27 10:05:14 -0500

    PLAYER AMAZING!

---
+++ pyramidGame/Assets/Scripts/BodyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMovement : MonoBehaviour {
+
+    public float moveVelocity;
+
+    public float maxVelocity;
+
+    Rigidbody thisRB;
+
+    GameObject head;
+
+    // Use this for initialization
+    void Start () {
+        thisRB = GetComponent<Rigidbody>();
+        head = GameObject.Find("Head");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 velocity = thisRB.velocity;
+        float velocityMag = (velocity.x + velocity.z) / 2;
+
+        transform.eulerAngles = new Vector3(0.0f, head.transform.eulerAngles.y, 0.0f);
+
+        if (Mathf.Abs(velocityMag) < maxVelocity)
+            thisRB.AddRelativeForce(
+                horizontalInput * moveVelocity,
+                0f,
+                verticalInput * moveVelocity,
+                ForceMode.Force
+            );
+	}
+}
