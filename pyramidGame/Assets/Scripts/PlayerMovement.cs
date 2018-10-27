708f8cb8ab582cecd77dfed791926dd8858bc839
Kaden Kilburg <kmkilburg28@gmail.com>
2018-10-27 00:19:05 -0500

    better player

--- pyramidGame/Assets/Scripts/PlayerMovement.cs
+++ pyramidGame/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,15 @@ public class PlayerMovement : MonoBehaviour {
 
     Rigidbody thisRB;
 
+    Vector3 rbTurnVelocity;
+    Vector3 mousePosition;
+
 	// Use this for initialization
 	void Start ()
     {
         thisRB = GetComponent<Rigidbody>();
-	}
+        rbTurnVelocity = new Vector3(0, 1, 0);
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -25,5 +29,12 @@ public class PlayerMovement : MonoBehaviour {
             verticalInput * moveVelocity,
             ForceMode.Force
         );
+
+        mousePosition = Input.mousePosition;
+        mousePosition.x -= Screen.width / 2;
+        float intensityX = (mousePosition.x / (Screen.width / 2));
+
+        Quaternion deltaRotation = Quaternion.Euler(rbTurnVelocity * intensityX);
+        thisRB.MoveRotation(thisRB.rotation * deltaRotation);
     }
 }
