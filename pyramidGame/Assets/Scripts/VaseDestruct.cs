5b62c18d73240da3413923de7fbb1459b4e6728a
Kaden Kilburg <kmkilburg28@gmail.com>
2018-10-27 12:51:28 -0500

    Fancy dropped bone

--- pyramidGame/Assets/Scripts/VaseDestruct.cs
+++ pyramidGame/Assets/Scripts/VaseDestruct.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructible : MonoBehaviour {
+public class VaseDestruct : MonoBehaviour {
 
     public GameObject destroyedVersion;
 
