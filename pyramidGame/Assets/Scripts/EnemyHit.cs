5b1d4bebe637a00ca8977e6961ee16b65e41ec34
zahydeem <44513031+zahydeem@users.noreply.github.com>
2018-10-28 10:47:24 -0400

    Merge branch 'master' of https://github.com/kmkilburg28/pyramidGame

--- pyramidGame/Assets/Scripts/EnemyHit.cs
+++ pyramidGame/Assets/Scripts/EnemyHit.cs
@@ -3,24 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHit : MonoBehaviour {
+
     GManager GManager;
+
+    private bool isInside;
+
+    IEnumerator thisCoroutine;
+
     // Use this for initialization
     void Start () {
         GManager = GameObject.Find("GManager").GetComponent<GManager>();
+        isInside = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("player"))
         {
             
-            GManager.numBones--;
+            if (!isInside)
+            {
+                isInside = true;
+                thisCoroutine = InsidePlayer();
+                StartCoroutine(thisCoroutine);
+            }
 
         }
-       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            isInside = false;
+        }
+    }
+
+    IEnumerator InsidePlayer()
+    {
+        while (isInside)
+        {
+            GManager.numBones--;
+            yield return new WaitForSeconds(1);
+        }
+        if (!isInside)
+        {
+            StopCoroutine(thisCoroutine);
+            yield return null;
+        }
     }
 }
